74bf42dd72143db0036b7ea61994a6a3d09f4b6e
Purav Nisar <36162246+Puravnisar@users.noreply.github.com>
2020-07-18 20:35:32 -0700

    initial conversion to cnf

--- Resolution.py
+++ Resolution.py
@@ -19,6 +19,21 @@ for i in range(1,n+1):
 k=int(data1[n+1])
 kbbefore=list()
 
+
+def CNF(sentence):
+    temp=re.split("=>",sentence)
+    temp1=temp[0].split('&')
+    for i in range(0,len(temp1)):
+        if temp1[i][0]=='~':
+            temp1[i]=temp1[i][1:]
+        else:
+            temp1[i]='~'+temp1[i]
+    temp2='|'.join(temp1)
+    temp2=temp2+'|'+temp[1]
+    return temp2
+
+
+
 if __name__ == '__main__': 
     finalanswer=resolution()
     o=open("output.txt","w+")
